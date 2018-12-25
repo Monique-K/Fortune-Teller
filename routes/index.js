d30a1bcca0741f60b1a2a9cf2f7a6dacfddd59ff
Monique-K <mkhoury87@hotmail.com>
2018-12-24 20:10:30 -0500

    working on searching for tweets

--- routes/index.js
+++ routes/index.js
@@ -2,40 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 
-// (function($,W,D)
-// {
-//   W.JQUERY4U = W.JQUERY4U || {};
-//   W.JQUERY4U.TWITTER = {
-//     name: "JQUERY4U TWITTER",
-//     init: function(wid)
-//     {
-//       //load twitter stylesheet
-//       $("head").append('');
-//       //get the tweets from Twitter API
-//       $.getJSON("http://search.twitter.com/search.json?q=fortune&rpp=5&callback=?", function(data)
-//       {
-//         // console.log(data.items.length);
-//         $(data.results).each(function(i,v)
-//         {
-//           var tweet=''+this.text.linkify().linktag().replace(/'+tweetBy+'';
-
-//           $('#twitter').append(tweet); //add the tweet...
-
-//           });
-//         });
-//       }
-//     }
-
-// })(jQuery,window,document);
-
-
 const getTweets = () => {
+  console.log("getting tweeeeeeeeeeets!  ")
   const path = "twurl /1.1/search/tweets.json?q=fortune";
   router.get(path, function(req, res, next) {
+    console.log("response", res)
     return res
   }) 
 }
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
